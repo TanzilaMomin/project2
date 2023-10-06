358cfe247eb912a793d8d7a2f5fc0d7466579f54
TanzilaMomin <tanzila.momin01@gmail.com>
2023-10-06 16:48:34 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className='main'>
+        <h1>School Days</h1>
+        <div className='sub'>
+          <h5>School Friends :</h5>
+          <ol>
+            <li>Rinky</li>
+            <li>Afreen</li>
+            <li>Minaz</li>
+            <li>Zeenat</li>
+          </ol>
+        </div>
+        <div className='sub'>
+          <h5>School Routine :</h5>
+          <ol type='a'>
+            <li> I used to Woke up Early morning</li>
+            <li>I used to take bath</li>
+            <li>I used to brush my teeth</li>
+            <li>I used to take My breakfast</li>
+            <li>I used to cath my bus</li>
+            <li>I used to enjoy my day at school</li>
+            <li>I used to cath bus to back to home</li>
+          </ol>
+        </div>
+        <div className='sub'>
+          <h5>School Games :</h5>
+          <ol>
+            <li>Cricket</li>
+            <li>Kabaddi</li>
+            <li>Kho-Kho</li>
+            <li>Football</li>
+            <li>Bad-minton</li>
+          </ol>
+        </div>
+        <div className='sub'>
+          <h5>Subject :</h5>
+          <ol>
+            <li>Maths</li>
+            <li>Urdu</li>
+            <li>English</li>
+            <li>Arbi</li>
+            <li>Science</li>
+            <li>Social Service</li>
+          </ol>
+        </div>
+      </div>
+  );
+}
+
+export default App;
